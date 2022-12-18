450566d6990ba8392adc05a14f8beeb098aa449c
elzp <86740659+elzp@users.noreply.github.com>
2022-12-18 17:33:59 +0100

    Add code to stick navbar to top while scrolling

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 import './index.css';
-console.log('Working'); 
+console.log('Working');
+
+const navbar = document.querySelector('.navbar');
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY === 0) {
+    navbar.classList.remove('fixed');
+  }
+  if (window.scrollY > 0) {
+    navbar.classList.add('fixed');
+  }
+});
