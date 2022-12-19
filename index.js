cc194f54bb508f6a0ab5221579af73c6a898d700
elzp <86740659+elzp@users.noreply.github.com>
2022-12-19 21:31:53 +0100

    Add click event to close menu in mobile view

--- index.js
+++ index.js
@@ -14,10 +14,23 @@ document.addEventListener('scroll', () => {
 
 const menuLines = document.querySelector('.navbar__lines');
 const navbarMenu = document.querySelector('.navbar__menu');
+const close = document.querySelector('.navbar__close');
 
 menuLines.addEventListener('click', (e) => {
   menuLines.classList.remove('visible');
   navbarMenu.classList.remove('hidden');
+  close.classList.remove('hidden');
+
   menuLines.classList.add('hidden');
   navbarMenu.classList.add('visible');
+  close.classList.add('visible');
+});
+
+close.addEventListener('click', (e) => {
+  navbarMenu.classList.remove('visible');
+  close.classList.remove('visible');
+  menuLines.classList.remove('hidden');
+  navbarMenu.classList.add('hidden');
+  close.classList.add('hidden');
+  menuLines.classList.add('visible');
 });
